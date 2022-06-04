592a6adb5deadb1d2249e414c642fad3262bce64
Sophia Freire <105390215+Sophia-Freire@users.noreply.github.com>
2022-06-03 22:36:19 -0300

    Add files via upload

---
+++ src/Questao01.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Questao01 {
+
+	public static void main(String[] args) {
+		// Escreva um algoritmo que leia dois números inteiros, calcule e mostre na tela a soma
+		//deles com uma mensagem informando a operação realizada.
+		
+		Scanner leia = new Scanner (System.in);
+		
+		int x;
+		int y;
+		int resultado; 
+		
+		System.out.println("Informe um numero:");
+		x = leia.nextInt();
+		System.out.println("Informe mais um numero:");
+		y = leia.nextInt();
+		
+		resultado = ( x + y);
+		
+		System.out.println("A soma e igual a: " + resultado);
+		
+	
+		
+		
+		
+	}
+
+}
